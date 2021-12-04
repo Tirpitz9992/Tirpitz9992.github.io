0698a4cb3fd9de3e8249634d549b532e43f0b3ce
Tirpitz9992 <tirpitz9992@gmail.com>
2021-12-05 00:02:30 +0800

    tiz_12_4_1

---
+++ profile/profile.js
@@ -0,0 +1,8 @@
+document.getElementById('content').innerHTML =
+     marked.parse(`# Introduction
+
+I am a trivial second-year undergraduate in physics(╹▽╹), just kind of understand computer science
+
+
+
+`);
